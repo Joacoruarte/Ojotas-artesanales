e74830fa09016c99c5a4d496e20012b6b388f7c3
Joacoruarte <ruartejoaquin@gmail.com>
2022-11-18 11:49:17 -0300

    enviroment log

--- pages/api/products.js
+++ pages/api/products.js
@@ -4,6 +4,7 @@ import Product from "../../models/Product.js"
 dbConnect()
 
 export default async function getProducts(req, res) {
+    console.log("ENVIROMENT", process.env.MONGODB_URL);
     const products = await Product.find({})
-    res.status(200).json({ success: true, count: products.length , data: products ,  })
+    res.status(200).json({ success: true, count: products.length , data: products })
 }
