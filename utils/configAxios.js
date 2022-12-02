90dac40d7f3c7565670f20a5df9986b9ef53c7c3
Joacoruarte <ruartejoaquin@gmail.com>
2022-12-01 23:21:28 -0300

    delete header in config axios

--- utils/configAxios.js
+++ utils/configAxios.js
@@ -2,9 +2,6 @@ import axios from "axios"
 
 const instance = axios.create({
     baseURL: process.env.NODE_ENV === "production" ? process.env.URI : process.env.URI_DEVELOP,
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 export default instance;
