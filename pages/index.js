7609aaa8a73ef80dec25bb7c6b69c8ed35751eb6
Joacoruarte <ruartejoaquin@gmail.com>
2022-12-01 21:14:12 -0300

    delete api

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home({ products }) {
 }
 
 export async function getServerSideProps() {
-  const res = await axios.get("/api/products")
+  const res = await axios.get("/products")
   const products = await res.data.data;
 
   return {
